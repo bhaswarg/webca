a08c096da97bd6dbca986f1ee798df59a2be9d5a
unknown <dbarrychef@gmail.com>
2020-12-17 16:04:23 +0000

    added final comments

--- public/script/d-script.js
+++ public/script/d-script.js
@@ -1,4 +1,5 @@
-//
+//Dom pass the element with designated id
+//if the element is invisable display it otherwise hide it.
 function toggleElement(id)
 {
     if(document.getElementById(id).style.display == 'none')
